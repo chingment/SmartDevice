dc179c9e95d36bda96f4698bfc308dcf8aec45dc
qqw <qqw@uplink.net.cn>
2021-07-09 22:18:48 +0800

    dd

--- app/src/main/java/com/lumos/smartdevice/ui/dialog/CustomDialogCabinetConfig.java
+++ app/src/main/java/com/lumos/smartdevice/ui/dialog/CustomDialogCabinetConfig.java
@@ -3,6 +3,7 @@ package com.lumos.smartdevice.ui.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.lumos.smartdevice.R;
 import com.lumos.smartdevice.model.CabinetBean;
 import com.lumos.smartdevice.ui.ViewHolder;
@@ -47,11 +50,11 @@ public class CustomDialogCabinetConfig extends Dialog {
 
     public CustomDialogCabinetConfig(Context context) {
         super(context, R.style.custom_dialog_confirm);
-        mThis=this;
+        mThis = this;
         mContext = context;
         mLayoutRes = LayoutInflater.from(context).inflate(R.layout.custom_dialog_cabinetconfig, null);
 
-        btn_Close =ViewHolder.get(mLayoutRes,R.id.dialog_Btn_Close);
+        btn_Close = ViewHolder.get(mLayoutRes, R.id.dialog_Btn_Close);
         btn_Close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,12 +62,13 @@ public class CustomDialogCabinetConfig extends Dialog {
             }
         });
 
-        tv_CabinetId =ViewHolder.get(mLayoutRes,R.id.tv_CabinetId);
-        tv_CabinetName =ViewHolder.get(mLayoutRes,R.id.tv_CabinetName);
-        tv_ComId =ViewHolder.get(mLayoutRes,R.id.tv_ComId);
-        tv_ComBaud =ViewHolder.get(mLayoutRes,R.id.tv_ComBaud);
-        tv_ComPrl =ViewHolder.get(mLayoutRes,R.id.tv_ComPrl);
-        tv_Layout =ViewHolder.get(mLayoutRes,R.id.tv_Layout);
+        tv_CabinetId = ViewHolder.get(mLayoutRes, R.id.tv_CabinetId);
+        tv_CabinetName = ViewHolder.get(mLayoutRes, R.id.tv_CabinetName);
+        tv_ComId = ViewHolder.get(mLayoutRes, R.id.tv_ComId);
+        tv_ComBaud = ViewHolder.get(mLayoutRes, R.id.tv_ComBaud);
+        tv_ComPrl = ViewHolder.get(mLayoutRes, R.id.tv_ComPrl);
+        tv_Layout = ViewHolder.get(mLayoutRes, R.id.tv_Layout);
+        tv_Layout.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 
     @Override
@@ -73,13 +77,26 @@ public class CustomDialogCabinetConfig extends Dialog {
         this.setContentView(mLayoutRes);
     }
 
-    public void setCofing(CabinetBean cabinet){
+    public void setCofing(CabinetBean cabinet) {
         tv_CabinetId.setText(cabinet.getCabinetId());
         tv_CabinetName.setText(cabinet.getName());
         tv_ComId.setText(cabinet.getComId());
-        tv_ComBaud.setText(cabinet.getComBaud()+"");
+        tv_ComBaud.setText(cabinet.getComBaud() + "");
         tv_ComPrl.setText(cabinet.getComPrl());
-        tv_Layout.setText(cabinet.getLayout());
+        String layout=prettyJson(cabinet.getLayout()) ;
+        tv_Layout.setText(layout);
+    }
+
+    private String prettyJson(String json) {
+
+
+        JSONArray jsonObject = null;
+        try {
+            jsonObject = JSONObject.parseArray(json);
+        } catch (Exception ex) {
+        }
+
+         return JSONArray.toJSONString(jsonObject,true);
     }
 
 }
