213ea00bdf7dd2c34080913acdea87dc47244748
qqw <qqw@uplink.net.cn>
2021-07-15 22:53:42 +0800

    dd

---
+++ app/src/main/java/com/lumos/smartdevice/db/LockerBoxDao.java
@@ -0,0 +1,11 @@
+package com.lumos.smartdevice.db;
+
+public class LockerBoxDao {
+    public static final String TABLE_NAME = "tb_lockerbox";
+    public static final String COLUMN_NAME_CABINET_ID= "cabinet_id";
+    public static final String COLUMN_NAME_SLOT_ID = "slot_id";
+    public static final String COLUMN_NAME_IS_ENABLE= "is_enable";
+    public static final String COLUMN_NAME_IS_USED = "is_used";
+    public static final String COLUMN_NAME_MODE = "mode";
+    public static final String COLUMN_NAME_REMARK= "remark";
+}
