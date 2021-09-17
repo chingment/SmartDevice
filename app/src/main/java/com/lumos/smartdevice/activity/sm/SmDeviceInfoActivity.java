2ca5b55f2f610eccc9c35b387a923880dca1a9e8
qqw <qqw@uplink.net.cn>
2021-09-17 15:41:57 +0800

    dd

--- app/src/main/java/com/lumos/smartdevice/activity/sm/SmDeviceInfoActivity.java
+++ app/src/main/java/com/lumos/smartdevice/activity/sm/SmDeviceInfoActivity.java
@@ -2,15 +2,70 @@ package com.lumos.smartdevice.activity.sm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.lumos.smartdevice.R;
+import com.lumos.smartdevice.ui.BaseFragmentActivity;
+import com.lumos.smartdevice.utils.NoDoubleClickUtil;
 
-public class SmDeviceInfoActivity extends AppCompatActivity {
+public class SmDeviceInfoActivity extends BaseFragmentActivity {
+
+    private static final String TAG = "SmLockerBoxActivity";
+
+
+    private TextView tv_MerchName;
+    private TextView tv_StoreName;
+    private TextView tv_ShopName;
+    private TextView tv_Address;
+    private TextView tv_DeviceId;
+    private TextView tv_ComName;
+    private TextView tv_AppVersion;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smdeviceinfo);
+
+        setNavHeaderTtile(R.string.aty_deviceinfo_nav_title);
+        setNavHeaderBtnByGoBackIsVisible(true);
+
+        initView();
+        initEvent();
+        initData();
+    }
+
+    private void initView() {
+        tv_MerchName = findViewById(R.id.tv_MerchName);
+        tv_StoreName = findViewById(R.id.tv_StoreName);
+        tv_ShopName = findViewById(R.id.tv_ShopName);
+        tv_Address = findViewById(R.id.tv_Address);
+        tv_DeviceId = findViewById(R.id.tv_DeviceId);
+        tv_ComName = findViewById(R.id.tv_ComName);
+        tv_AppVersion = findViewById(R.id.tv_AppVersion);
+    }
+
+    private void initEvent() {
+
+    }
+
+    private void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (!NoDoubleClickUtil.isDoubleClick()) {
+            switch (v.getId()) {
+                case R.id.btn_Nav_Header_Goback:
+                    finish();
+                    break;
+            }
+        }
     }
 }
