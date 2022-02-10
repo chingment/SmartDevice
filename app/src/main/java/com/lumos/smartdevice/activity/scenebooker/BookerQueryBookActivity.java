e2a857482d8d2e9ef47520b757ca4cac14db08ff
qqw <qqw@uplink.net.cn>
2022-02-10 17:47:50 +0800

    s

---
+++ app/src/main/java/com/lumos/smartdevice/activity/scenebooker/BookerQueryBookActivity.java
@@ -0,0 +1,16 @@
+package com.lumos.smartdevice.activity.scenebooker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lumos.smartdevice.R;
+
+public class BookerQueryBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booker_query_book);
+    }
+}
